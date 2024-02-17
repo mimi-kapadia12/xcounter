43a3e4da376b1c422a660c5d79f5d5c064d3918f
mimi-kapadia12 <mansikapadia1999@gmail.com>
2024-02-17 21:34:22 +0530

    Fixed issues

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
 
   return (
     <div className="App">
-      <p>{count}</p>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
       <button onClick={Increment}>Increment</button>
       <button onClick={Decrement}>decrement</button>
     </div>
