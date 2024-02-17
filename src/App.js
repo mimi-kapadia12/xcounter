5c07096da25af8594bd3072c6d12bd0904072482
mimi-kapadia12 <mansikapadia1999@gmail.com>
2024-02-17 21:40:08 +0530

    Fixed issue

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <h1>Counter App</h1>
       <p>Count: {count}</p>
       <button onClick={Increment}>Increment</button>
-      <button onClick={Decrement}>decrement</button>
+      <button onClick={Decrement}>Decrement</button>
     </div>
   );
 }
